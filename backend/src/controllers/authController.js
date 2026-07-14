c22df541403ef9ecfa8360dcfe7c0670ae692e90
Feberlys Batista <feberlysbatistaramirez@gmail.com>
2026-07-13 22:45:11 -0400

    Nose cuantas veces voy a cabiar esto

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,7 +1,3 @@
-// Agrega esto temporalmente en tu función login, justo antes de bcrypt.compare:
-console.log("Hash en DB para este usuario:", usuario.PasswordHash);
-const hashPrueba = await bcrypt.hash('vetmanager123', 10);
-console.log("Hash generado en tiempo real:", hashPrueba);
 
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
